7b90000c95e84221173192cb54d093c40ecbdbf7
Sonja Blumenstock <blumenstock@neuro.mpg.de>
2020-03-23 16:57:10 +0100

    removed unnessessary variables

--- mean_pixels.py
+++ mean_pixels.py
@@ -1,23 +1,20 @@
 # This is my project
 
-#Python knows how to du tuple unpacking for setting data
+# Python knows how to du tuple unpacking for setting data
 # e.g. x, y = 1, 2
 
 import statistics
 
+
 def calculate_pixel_brightness(x, y, z):
-    brightness = statistics.mean([x, y, z])
-    return brightness
+    return statistics.mean([x, y, z])
 
 
-pixel = {'red': 100, 'green': 0, 'blue': 0}
-pixel2 = {'red': 50, 'green': 100, 'blue': 0}
-pixels = [pixel, pixel2]
+pixels = [
+    {'red': 100, 'green': 0, 'blue': 0},
+    {'red': 50, 'green': 100, 'blue': 0},
+]
 
 pixel_brightnesses = [calculate_pixel_brightness(pixel['red'], pixel['green'], pixel['blue']) for pixel in pixels]
-
 mean_brightness = statistics.mean(pixel_brightnesses)
 print(mean_brightness)
-
-
-
