75aaf10d4aa3f2c8957484c5ba0776efe6470d23
Sonja Blumenstock <blumenstock@neuro.mpg.de>
2020-03-23 16:20:35 +0100

    make code 'dry' (don't repeat yourself)

--- mean_pixels.py
+++ mean_pixels.py
@@ -3,11 +3,17 @@
 #Python knows how to du tuple unpacking for setting data
 # e.g. x, y = 1, 2
 
+
+def calculate_pixel_brightness(x, y, z):
+    brightness = (x + y + z)/3
+    return brightness
+
+
 red, green, blue = 100, 0, 0
-pixel_brightness = (red + green + blue) / 3
+pixel_brightness = calculate_pixel_brightness(red, green, blue)
 
 red2, green2, blue2 = 50, 100, 0
-pixel_brightness2 = (red2 + green2 + blue2) / 3
+pixel_brightness2 = calculate_pixel_brightness(red2, green2, blue2)
 
 readiness = 0
 readiness2 = 200
