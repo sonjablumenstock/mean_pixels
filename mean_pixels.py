377fab2c178e9b22ac5849ba33bdde8635a5b4df
Sonja Blumenstock <blumenstock@neuro.mpg.de>
2020-03-23 15:40:29 +0100

    made variable names interpretable

--- mean_pixels.py
+++ mean_pixels.py
@@ -1,17 +1,17 @@
 # This is my project
 
-x = 100  # Red value
-x2 = 50
-y = 0  # Green value
-y2 = 100
-z = 0  # Blue value
-z2 = 0
-w = 0  # Readiness value
-w2 = 200
+RedValue = 100
+RedValue2 = 50
+GreenValue = 0
+GreenValue2 = 100
+BlueValue = 0
+BlueValue2 = 0
+ReadinessValue = 0
+ReadinessValue2 = 200
 
 # Get pixel brightnesses
-result = (x + y + z) / 3
-result2 = (x2 + y2 + z2) / 3
+result = (RedValue + GreenValue + BlueValue) / 3
+result2 = (RedValue2 + GreenValue2 + BlueValue2) / 3
 
 # Get mean brightness
 result3 = (result + result2) / 2
