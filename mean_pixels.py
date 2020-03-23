cb497cd565b95edf939488bd2b4846bee72a42f8
Sonja Blumenstock <blumenstock@neuro.mpg.de>
2020-03-23 17:15:42 +0100

    split off utils module

--- mean_pixels.py
+++ mean_pixels.py
@@ -1,10 +1,6 @@
 # This is my project
 import statistics
-
-
-def calculate_pixel_brightness(red: int, green: int, blue: int) -> float:
-    """Calculates the brightness of a pixel from its r, g, b values"""
-    return statistics.mean([red, green, blue])
+from utils import calculate_pixel_brightness
 
 
 pixels = [
