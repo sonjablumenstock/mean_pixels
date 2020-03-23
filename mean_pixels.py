239c20f92bdf77ea7e416f1e1b95cf4a9b51747e
Sonja Blumenstock <blumenstock@neuro.mpg.de>
2020-03-23 16:12:45 +0100

    used tuple unpacking for assigning values

--- mean_pixels.py
+++ mean_pixels.py
@@ -1,18 +1,19 @@
 # This is my project
 
-red = 100
-red2 = 50
-green = 0
-green2 = 100
-blue = 0
-blue2 = 0
+#Python knows how to du tuple unpacking for setting data
+# e.g. x, y = 1, 2
+
+red, green, blue = 100, 0, 0
+pixel_brightness = (red + green + blue) / 3
+
+red2, green2, blue2 = 50, 100, 0
+pixel_brightness2 = (red2 + green2 + blue2) / 3
+
 readiness = 0
 readiness2 = 200
 
-
-pixel_brightness = (red + green + blue) / 3
-pixel_brightness2 = (red2 + green2 + blue2) / 3
-
-
 mean_brightness = (pixel_brightness + pixel_brightness2) / 2
 print(mean_brightness)
+
+
+
